e9217c9d4baf8cab33fba3ce5fdc06164c2590c4
Nicolas Delauney <niko-d@live.fr>
2020-12-01 09:46:52 +0100

    1️⃣ - 2️⃣: Day 1 - Part 2

--- 2020/2020.test.ts
+++ 2020/2020.test.ts
@@ -1,4 +1,4 @@
-import { day1_1, findFirstCoupleThatSumsTo } from './day1'
+import { day1_1, day1_2, findFirstCoupleThatSumsTo } from './day1'
 
 describe('Advent of Code 2020', () => {
 	describe('Day 1', () => {
@@ -11,12 +11,15 @@ describe('Advent of Code 2020', () => {
 
 			it('should find unordered', () => {
 				const testCase = findFirstCoupleThatSumsTo(10)
-				const given = [12,3,4,5,6,7, 1,8,9]
+				const given = [12,3,4,5,6,7,8,9]
 				expect(testCase(given)).toEqual([3, 7])
 			})
 		})
 		it('Part 1 should be equal to 381699', () => {
 			expect(day1_1()).toBe(381699)
+		})
+		it('Part 2 should be equal to 111605670', () => {
+			expect(day1_2()).toBe(111605670)
 		})
 	})
 })
--- 2020/day1.ts
+++ 2020/day1.ts
@@ -15,13 +15,35 @@ export const findFirstCoupleThatSumsTo = (num: number): (entries: number[]) => n
 				return [value, second]
 			}
 		}).filter(value => value?.every(isDefined))[0]
-	if(result?.every(isDefined)) {
-		return result
+
+		if(result?.every(isDefined)) {
+			return result
+		}
+
+		throw 'No matching pair in entries'
 	}
-	throw 'No matching pair in entries'
-}
 }
 
+export const findFirstTrioThatSumsTo = (num: number): (entries: number[]) => number[] => {
+	return entries => {
+		const result = entries.map((value: number, index: number, array: number[]) => {
+			try {
+				const second = findFirstCoupleThatSumsTo(num - value)(array.filter(isNotIndex(index)))
+				if(second?.every(isDefined)) {
+					return [value, ...second]
+				}
+			} catch(e) {
+				return null
+			}
+		}).filter(value => value?.every(isDefined))[0]
+
+		if(result?.every(isDefined)) {
+			return result
+		}
+
+		throw 'No matching trio in entries'
+	}
+}
 
 export const day1_1 = (): number => {
 	const entries = getInputFrom('day1').map((it: string) => parseInt(it))
@@ -29,4 +51,11 @@ export const day1_1 = (): number => {
 	return (sumTo2020(entries) as number[]).reduce(times, 1)
 }
 
+export const day1_2= (): number => {
+	const entries = getInputFrom('day1').map((it: string) => parseInt(it))
+	const sumTo2020 = findFirstTrioThatSumsTo(2020)
+	return (sumTo2020(entries) as number[]).reduce(times, 1)
+}
+
 console.log(`Day 1.1 results is : \t` , day1_1())
+console.log(`Day 1.2 results is : \t` , day1_2())
