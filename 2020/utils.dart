f809b3e45af9ee1dbfe49fc8bf18ddecd6d235a3
Nicolas Delauney <niko-d@live.fr>
2020-12-07 17:22:22 +0100

    🎄 refactor day 6

--- 2020/utils.dart
+++ 2020/utils.dart
@@ -1,6 +1,7 @@
 library utils;
 
 import 'dart:io';
+import 'package:meta/meta.dart';
 
 import "package:path/path.dart" show dirname, join;
 
@@ -9,8 +10,42 @@ Future<List<String>> getInputForDay(int day) async {
   return (await new File(path).readAsLines()).toList();
 }
 
+const ANSI_BOLD_YELLOW = '\x1B[1;93m';
+const ANSI_RESET = '\x1B[0m';
+
 void printResult(String part, dynamic result) {
-  const ANSI_BOLD_YELLOW = '\x1B[1;93m';
-  const ANSI_RESET = '\x1B[0m';
   print('Part $part result is $ANSI_BOLD_YELLOW$result$ANSI_RESET');
+}
+
+class AdventOutput {
+  static const int width = 42;
+  int day;
+  int Function(List<String>) part1;
+  int Function(List<String>) part2;
+  List<String> Function(List<String>) pipe;
+
+  AdventOutput(
+      {@required this.day,
+      @required this.part1,
+      @required this.part2,
+      this.pipe}) {
+    run();
+  }
+
+  void run() async {
+    final separators =
+        List.generate(((width - 8) / 2).floor(), (_) => '-').join();
+    final title = 'DAY ' + (day > 10 ? '' : '0');
+    print('$separators $title$day $separators');
+    var input = (await getInputForDay(day)).toList();
+    if (pipe != null) {
+      input = pipe(input);
+    }
+    final result1 = await Future.microtask(() => part1(input));
+    print('Part 1 :'.padRight(width - result1.toString().length) +
+        '$ANSI_BOLD_YELLOW$result1$ANSI_RESET');
+    final result2 = await Future.microtask(() => part2(input));
+    print('Part 2 :'.padRight(width - result2.toString().length) +
+        '$ANSI_BOLD_YELLOW$result2$ANSI_RESET');
+  }
 }
