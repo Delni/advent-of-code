8b98ae5077a9ddc8a31e6154074f815416a7af9f
Nicolas DELAUNEY <niko-d@live.fr>
2021-12-02 22:22:28 +0100

    2️⃣ Refactor with submarine class

--- 2021/src/main/kotlin/com/github/delni/advent_of_code/days/template.kt
+++ 2021/src/main/kotlin/com/github/delni/advent_of_code/days/template.kt
@@ -21,23 +21,24 @@ abstract class AbstractDay(
     }
 
 
-    fun run() {
+    private fun run() {
         val testInput = readInput("Day${DAY}_test")
         val testResult1 = part1(testInput)
         val testResult2 = part2(testInput)
         assert(TEST_VALUE_1?.let { testResult1 == TEST_VALUE_1} ?: true) {
-            "Expected $TEST_VALUE_1, got was $testResult1"
+            "Expected $TEST_VALUE_1, got $testResult1"
         }
         assert(TEST_VALUE_2?.let { testResult2 == TEST_VALUE_2} ?: true) {
             "Expected $TEST_VALUE_2, got $testResult2"
         }
 
         val input = readInput("Day${DAY}")
-        val emphasizedText = "DAY $DAY : $TITLE".green()
+        val emphasizedText = "DAY$DAY : $TITLE".green()
         val title = "+-------- $emphasizedText --------+"
-        println(title)
         val part1Result = part1(input).toString().yellow()
         val part2Result = part2(input).toString().yellow()
+
+        println(title)
         println("| Part 01 ".padEnd(title.length - part1Result.length - 3, ' ') + " $part1Result |")
         println("| Part 02 ".padEnd(title.length - part2Result.length - 3, ' ') + " $part2Result |")
         println("".cyan().padEnd(title.length, '-'))
