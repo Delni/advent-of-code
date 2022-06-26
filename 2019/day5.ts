ad492030c7b70bfbf48c29c45ceb5c93dec77792
Nicolas DELAUNEY <niko-d@live.fr>
2022-06-26 18:04:35 +0200

    ⛄️ Day 05

---
+++ 2019/day5.ts
@@ -0,0 +1,14 @@
+import { getInputFrom, prettyPrint, sum, toInt } from './utils.ts';
+
+const input = await getInputFrom('day5', ',');
+
+const part1 = (input: string[]) => 1;
+const part2 = (input: string[]) => 1;
+
+prettyPrint('05', [{
+  prefix: 'Part 1',
+  result: part1(input)
+}, {
+  prefix: 'Part 2',
+  result: part1(input)
+}])
