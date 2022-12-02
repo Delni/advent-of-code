c178958c7469c2ecb8a83756c9ce844c0e53d429
Nicolas DELAUNEY <niko-d@live.fr>
2022-12-02 22:30:22 +0100

    🎁 refactor day 2

--- 2022/src/main/kotlin/com/github/delni/adventOfCode2022/days/Day2.kt
+++ 2022/src/main/kotlin/com/github/delni/adventOfCode2022/days/Day2.kt
@@ -1,75 +1,44 @@
 package com.github.delni.adventOfCode2022.days
 
+import com.github.delni.adventOfCode2022.entities.*
+
 class Day2 : Abstract2022("02", "Rock Paper Scissors") {
     override fun part1(input: List<String>): Int = input
-        .asSequence()
-        .map { it.split(" ") }
-        .map { Shape.parseString(it[0]) to Shape.parseString(it[1]) }
+        .normalize { it[1].toShape() to it[0].toShape() }
         .map(::runGame)
         .sum()
 
     override fun part2(input: List<String>): Int = input
-        .asSequence()
-        .map { it.split(" ") }
-        .map { Shape.parseString(it[0]) to it[1] }
-        .map { it.first to Shape.findOutput(it.second, it.first) }
+        .normalize { it[1] to it[0].toShape() }
+        .map { it.first toShapeAgainst it.second }
         .map(::runGame)
         .sum()
 
+    private fun <R> List<String>.normalize(transform: (List<String>) -> Pair<R, Shape>) = asSequence()
+        .map { it.split(" ") }
+        .map(transform)
+
     private fun runGame(round: Pair<Shape, Shape>): Int =
-        round.second.points + when (round.second.compareTo(round.first)) {
-            0 -> 3
-            1 -> 6
-            else -> 0
+        round.first.points + (round.first against round.second)
+
+    private fun String.toShape() = when (this) {
+        "A" -> Rock()
+        "B" -> Paper()
+        "C" -> Scissors()
+        // For part 1 only
+        "X" -> Rock()
+        "Y" -> Paper()
+        "Z" -> Scissors()
+        else -> throw NoWhenBranchMatchedException(this)
+    }
+
+    private infix fun String.toShapeAgainst(other: Shape): Pair<Shape, Shape> = Shape.allShapes.first {
+        when (this) {
+            "X" -> it loseAgainst other
+            "Y" -> it drawAgainst other
+            "Z" -> it winAgainst other
+            else -> throw NoWhenBranchMatchedException(this)
         }
+    } to other
 }
 
-sealed class Shape(val points: Int) : Comparable<Shape> {
-    companion object {
-        fun parseString(str: String) = when (str) {
-            "A" -> Rock()
-            "B" -> Paper()
-            "C" -> Scissor()
-            // For part 1 only
-            "X" -> Rock()
-            "Y" -> Paper()
-            "Z" -> Scissor()
-            else -> throw NoWhenBranchMatchedException(str)
-        }
-        fun findOutput(str: String, other: Shape) = listOf(Rock(), Paper(), Scissor()).first {
-            when (str) {
-                "X" ->  it < other
-                "Y" -> it.compareTo(other) == 0
-                "Z" ->  it > other
-                else -> throw NoWhenBranchMatchedException(str)
-            }
-        }
-    }
-}
-
-class Rock : Shape(1) {
-    override fun compareTo(other: Shape) = when (other) {
-        is Paper -> -1
-        is Rock -> 0
-        is Scissor -> 1
-    }
-
-}
-
-class Paper : Shape(2) {
-    override fun compareTo(other: Shape) = when (other) {
-        is Paper -> 0
-        is Rock -> 1
-        is Scissor -> -1
-    }
-
-}
-
-class Scissor : Shape(3) {
-    override fun compareTo(other: Shape) = when (other) {
-        is Paper -> 1
-        is Rock -> -1
-        is Scissor -> 0
-    }
-
-}
