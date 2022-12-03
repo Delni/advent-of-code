0ccbdfef6f80a81e81de264a716216ff8359f25e
Nicolas DELAUNEY <niko-d@live.fr>
2022-12-03 18:40:52 +0100

    🎁🚧 prepare day 4

---
+++ 2022/kotlin-lang/src/main/kotlin/mmxxii/days/Day4.kt
@@ -0,0 +1,7 @@
+package mmxxii.days
+
+class Day4 : Abstract2022("04", "?") {
+    override fun part1(input: List<String>) = input.size
+
+    override fun part2(input: List<String>) = input.size
+}
---
+++ 2022/kotlin-lang/src/test/kotlin/mmxxii/days/Day4Test.kt
@@ -0,0 +1,22 @@
+package mmxxii.days
+
+import org.junit.jupiter.api.Test
+
+import kotlin.test.expect
+
+class Day4Test {
+
+    private val testInput = listOf(
+        ""
+    )
+
+    @Test
+    fun `part 1 should return 1 on test input`() = expect(1) {
+        Day4().part1(testInput)
+    }
+
+    @Test
+    fun `part 2 should return 1 on test input`() = expect(1) {
+        Day4().part2(testInput)
+    }
+}
