d44d1052d6e92395da81403638010812f70899eb
Nicolas DELAUNEY <niko-d@live.fr>
2022-12-03 18:12:52 +0100

    🚚 Move resources at year root

--- tools/src/main/kotlin/reader.kt
+++ tools/src/main/kotlin/reader.kt
@@ -3,4 +3,4 @@ import java.io.File
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String, year: Int) = File("$year/src/main/resources", "$name.txt").readLines()
+fun readInput(name: String, year: Int) = File("$year/resources", "$name.txt").readLines()
