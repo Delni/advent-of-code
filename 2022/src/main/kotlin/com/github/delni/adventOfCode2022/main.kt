f0b08d95894f6160cad1729265451d0aa27ceec7
Nicolas DELAUNEY <niko-d@live.fr>
2022-12-03 10:20:40 +0100

    🎁 Day 3 first draft

--- 2022/src/main/kotlin/com/github/delni/adventOfCode2022/main.kt
+++ 2022/src/main/kotlin/com/github/delni/adventOfCode2022/main.kt
@@ -2,8 +2,10 @@ package com.github.delni.adventOfCode2022
 
 import com.github.delni.adventOfCode2022.days.Day1
 import com.github.delni.adventOfCode2022.days.Day2
+import com.github.delni.adventOfCode2022.days.Day3
 
 fun main() {
     Day1().run()
     Day2().run()
+    Day3().run()
 }
