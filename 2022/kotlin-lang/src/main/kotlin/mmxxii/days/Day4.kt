2d49e4a014527b5900d0eda5f07cbbc0e3afdf86
Nicolas DELAUNEY <niko-d@live.fr>
2022-12-04 11:42:13 +0100

    🎁 Day 4

--- 2022/kotlin-lang/src/main/kotlin/mmxxii/days/Day4.kt
+++ 2022/kotlin-lang/src/main/kotlin/mmxxii/days/Day4.kt
@@ -1,7 +1,29 @@
 package mmxxii.days
 
-class Day4 : Abstract2022("04", "?") {
-    override fun part1(input: List<String>) = input.size
+class Day4 : Abstract2022("04", "Camp Cleanup") {
+    override fun part1(input: List<String>) = input
+        .map(String::toElvesPair)
+        .count(List<IntRange>::overlapCompletely)
 
-    override fun part2(input: List<String>) = input.size
+    override fun part2(input: List<String>) = input
+        .map(String::toElvesPair)
+        .count {
+            it.first()
+                .any { id -> id in it.last() } || it.last().any { id -> id in it.first() }
+        }
 }
+
+fun String.toElvesPair(): List<IntRange> {
+    val regex = Regex("(\\d+)-(\\d+)")
+    return regex.findAll(this).toList().map {
+        val bounds = it.groups
+            .drop(1)
+            .filterNotNull()
+            .map(MatchGroup::value)
+            .map(String::toInt)
+        IntRange(bounds.first(), bounds.last())
+    }
+}
+
+fun List<IntRange>.overlapCompletely() = first()
+    .all { id -> id in last() } || last().all { id -> id in first() }
--- 2022/kotlin-lang/src/test/kotlin/mmxxii/days/Day4Test.kt
+++ 2022/kotlin-lang/src/test/kotlin/mmxxii/days/Day4Test.kt
@@ -1,5 +1,6 @@
 package mmxxii.days
 
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 import kotlin.test.expect
@@ -7,16 +8,27 @@ import kotlin.test.expect
 class Day4Test {
 
     private val testInput = listOf(
-        ""
+        "2-4,6-8",
+        "2-3,4-5",
+        "5-7,7-9",
+        "2-8,3-7",
+        "6-6,4-6",
+        "2-6,4-8",
     )
 
     @Test
-    fun `part 1 should return 1 on test input`() = expect(1) {
+    fun `part 1 should return 2 on test input`() = expect(2) {
         Day4().part1(testInput)
     }
 
     @Test
-    fun `part 2 should return 1 on test input`() = expect(1) {
+    fun `part 2 should return 4 on test input`() = expect(4) {
         Day4().part2(testInput)
     }
+
+    @Test
+    fun `should parse string into elves pair of ranges`() = expect(listOf(2..4, 6..8)) {
+        "2-4,6-8".toElvesPair()
+    }
+
 }
