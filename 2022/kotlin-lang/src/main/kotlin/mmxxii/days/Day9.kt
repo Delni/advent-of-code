6fa0bfb55977dbf9b89fdec9ee50f2ffac326174
Nicolas DELAUNEY <niko-d@live.fr>
2022-12-09 09:16:25 +0100

    🎁 Refactor Day 9

--- 2022/kotlin-lang/src/main/kotlin/mmxxii/days/Day9.kt
+++ 2022/kotlin-lang/src/main/kotlin/mmxxii/days/Day9.kt
@@ -6,81 +6,63 @@ import kotlin.math.sqrt
 
 
 class Day9 : Abstract2022<Int>("09", "Rope Bridge") {
-    override fun part1(input: List<String>): Int {
-        val visited = mutableSetOf<Point>()
-        val head = Point(0, 0)
-        val tail = Point(0, 0)
-        val moveRegex = Regex("""(\w) (\d+)""")
-        input.forEach {
-            val direction = moveRegex.find(it)!!.groupValues[1]
-            val distance = moveRegex.find(it)!!.groupValues[2].toInt()
-            val dx = when (direction) {
-                "R" -> 1
-                "L" -> -1
-                else -> 0
-            }
-            val dy = when (direction) {
-                "U" -> -1
-                "D" -> 1
-                else -> 0
-            }
-            val currentHead = Point(head.x, head.y)
-            while (head.x != currentHead.x + dx * distance || head.y != currentHead.y + dy * distance) {
-                head.translate(dx, dy)
-                if (head.distance(tail) > sqrt(2.0)) {
-                    tail.translate((head.x - tail.x).sign, (head.y - tail.y).sign)
-                }
-                visited.add(Point(tail.x, tail.y))
+    override fun part1(input: List<String>) = makeRopeWithLength(2) movingAlongAndVisiting input
+
+    override fun part2(input: List<String>) = makeRopeWithLength(10) movingAlongAndVisiting input
+
+    private fun makeRopeWithLength(length: Int): List<Point> = List(length) { Point(0, 0) }
+
+    private infix fun List<Point>.movingAlongAndVisiting(input: List<String>) = mutableSetOf<Point>().apply {
+        input.forEach { string ->
+            string.asMove { move ->
+                moveAlong(move) { add(last().location) }
             }
         }
-        return visited.size
+    }.size
+
+    private fun <T> String.asMove(block: (Vector) -> T) {
+        val moveRegex = Regex("""(\w) (\d+)""")
+        val direction = moveRegex.find(this)!!.groupValues[1]
+        val distance = moveRegex.find(this)!!.groupValues[2].toInt()
+        val dx = when (direction) {
+            "R" -> 1
+            "L" -> -1
+            else -> 0
+        }
+        val dy = when (direction) {
+            "U" -> -1
+            "D" -> 1
+            else -> 0
+        }
+        block(Vector(dx, dy, distance))
     }
 
-    override fun part2(input: List<String>) : Int {
-        val visited = mutableSetOf<Point>()
-        val head = Point(0, 0)
-        val rope = listOf(
-            head,
-            Point(0, 0),
-            Point(0, 0),
-            Point(0, 0),
-            Point(0, 0),
-            Point(0, 0),
-            Point(0, 0),
-            Point(0, 0),
-            Point(0, 0),
-            Point(0, 0),
-        )
-        val moveRegex = Regex("""(\w) (\d+)""")
-        input.forEach {
-            val direction = moveRegex.find(it)!!.groupValues[1]
-            val distance = moveRegex.find(it)!!.groupValues[2].toInt()
-            val dx = when (direction) {
-                "R" -> 1
-                "L" -> -1
-                else -> 0
-            }
-            val dy = when (direction) {
-                "U" -> -1
-                "D" -> 1
-                else -> 0
-            }
-            val currentHead = Point(head.x, head.y)
-            while (head.x != currentHead.x + dx * distance || head.y != currentHead.y + dy * distance) {
-                head.translate(dx, dy)
-                rope.windowed(2).forEach { knots ->
-                    val previousKnot = knots.first()
-                    val knot = knots.last()
-                    if (previousKnot.distance(knot) > sqrt(2.0)) {
-                        knot.translate((previousKnot.x - knot.x).sign, (previousKnot.y - knot.y).sign)
-                    }
-                }
-                with(rope.last()) {
-                    visited.add(Point(x, y))
+    private fun <T> List<Point>.moveAlong(vector: Vector, sideEffect: List<Point>.() -> T) {
+        val head = first()
+        val target = Point(head.x + vector.x, head.y + vector.y)
+        while (head.x != target.x || (head.y != target.y)) {
+            head.translate(vector.dx, vector.dy)
 
-                }
+            windowed(2).forEach { knots ->
+                val previousKnot = knots.first()
+                val knot = knots.last()
+
+                takeIf { previousKnot.distance(knot) > sqrt(2.0) }
+                    ?.let { knot translateByOneToward previousKnot }
             }
+
+            sideEffect()
         }
-        return visited.size
+    }
+
+    private infix fun Point.translateByOneToward(point: Point) = translate((point.x - x).sign, (point.y - y).sign)
+
+
+    private data class Vector(val dx: Int, val dy: Int, private val norm: Int) {
+        val x
+            get() = dx * norm
+
+        val y
+            get() = dy * norm
     }
 }
