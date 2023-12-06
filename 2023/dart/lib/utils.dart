f0ef105386800a94e9569f36fb98aa9505539e43
Nicolas DELAUNEY <niko-d@live.fr>
2023-12-06 08:47:23 +0100

    🚚 move files to subfolders

--- 2023/dart/lib/utils.dart
+++ 2023/dart/lib/utils.dart
@@ -4,11 +4,12 @@ import 'dart:io';
 
 import "package:path/path.dart" show dirname, join;
 
+const resourcePath = "../../resources";
+
 Future<List<String>> getInputForDay(int day) async {
-  final path = join(dirname(Platform.script.path), '../inputs/day$day.txt');
+  final path = join(dirname(Platform.script.path), resourcePath, 'day$day.txt');
   return (await new File(path).readAsLines()).toList();
 }
-
 
 extension MathList on Iterable {
   num sum() => this.fold<num>(0, (p, e) => p + e);
